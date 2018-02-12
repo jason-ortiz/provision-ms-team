746853814ce1d8b0aab4a2f144fe6c65db8304b9
Jason Ortiz <jason.aaron.ortiz@gmail.com>
2018-02-12 13:23:16 -0600

    Update ProvisionMicrosoftTeam.ps1

--- ProvisionMicrosoftTeam.ps1
+++ ProvisionMicrosoftTeam.ps1
@@ -1,3 +1,10 @@
+<# Copyright (c) Microsoft Corporation. All rights reserved.
+   This code is licensed under the Microsoft Public License.
+   THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+   ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+   IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+   PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT. #>
+
 Param
 (
     [Parameter (Mandatory=$true)]
